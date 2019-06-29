380f6d0de7bcec8e8df992802e44211be1543723
tsuyoshi007 <45678324+tsuyoshi007@users.noreply.github.com>
2019-06-29 12:36:55 +0700

    update new version

--- app/src/main/java/com/test/tictactoe/MainActivity.kt
+++ app/src/main/java/com/test/tictactoe/MainActivity.kt
@@ -4,25 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var table: BooleanArray = booleanArrayOf(false, false, false, false, false, false, false, false, false)
+    var table: IntArray = intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
     var player: Boolean = false
-    val btn1 = findViewById<Button>(R.id.button1)
-    val btn2 = findViewById<Button>(R.id.button2)
-    val btn3 = findViewById<Button>(R.id.button3)
-    val btn4 = findViewById<Button>(R.id.button4)
-    val btn5 = findViewById<Button>(R.id.button5)
-    val btn6 = findViewById<Button>(R.id.button6)
-    val btn7 = findViewById<Button>(R.id.button7)
-    val btn8 = findViewById<Button>(R.id.button8)
-    val btn9 = findViewById<Button>(R.id.button9)
+    var turn = 0
+    var won = false
+    lateinit var btn1: Button
+    lateinit var btn2: Button
+    lateinit var btn3: Button
+    lateinit var btn4: Button
+    lateinit var btn5: Button
+    lateinit var btn6: Button
+    lateinit var btn7: Button
+    lateinit var btn8: Button
+    lateinit var btn9: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn1 = findViewById(R.id.button1)
+        btn2 = findViewById(R.id.button2)
+        btn3 = findViewById(R.id.button3)
+        btn4 = findViewById(R.id.button4)
+        btn5 = findViewById(R.id.button5)
+        btn6 = findViewById(R.id.button6)
+        btn7 = findViewById(R.id.button7)
+        btn8 = findViewById(R.id.button8)
+        btn9 = findViewById(R.id.button9)
+
         newGameButton.setOnClickListener {
+            turn = 0
+            won =false
             btn1.text = ""
             btn2.text = ""
             btn3.text = ""
@@ -32,103 +49,267 @@ class MainActivity : AppCompatActivity() {
             btn7.text = ""
             btn8.text = ""
             btn9.text = ""
+            btn1.isClickable = true
+            btn2.isClickable = true
+            btn3.isClickable = true
+            btn4.isClickable = true
+            btn5.isClickable = true
+            btn6.isClickable = true
+            btn7.isClickable = true
+            btn8.isClickable = true
+            btn9.isClickable = true
+            table = intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
         }
+    }
+
+    fun checkHorizontal(num: Int): Boolean {
+        return when (num) {
+            1 -> {
+                table[0] == table[1] && table[1] == table[2]
+            }
+            2 -> {
+                table[3] == table[4] && table[4] == table[5]
+            }
+            3 -> {
+                table[6] == table[7] && table[7] == table[8]
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    fun checkVertical(num: Int): Boolean {
+        return when (num) {
+            1 -> {
+                table[0] == table[3] && table[3] == table[6]
+            }
+            2 -> {
+                table[1] == table[4] && table[4] == table[7]
+            }
+            3 -> {
+                table[2] == table[5] && table[5] == table[8]
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    fun checkCross(num: Int): Boolean {
+        return when (num) {
+            1 -> {
+                table[0] == table[4] && table[4] == table[8]
+            }
+            2 -> {
+                table[2] == table[4] && table[4] == table[6]
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    fun disableAllButton() {
+        btn1.isClickable = false
+        btn2.isClickable = false
+        btn3.isClickable = false
+        btn4.isClickable = false
+        btn5.isClickable = false
+        btn6.isClickable = false
+        btn7.isClickable = false
+        btn8.isClickable = false
+        btn9.isClickable = false
+    }
+
+    fun message() {
+        won = true
+        if (player) {
+            Toast.makeText(
+                applicationContext,
+                "Player O win!!! Please press new game to play again",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Player X win!!! Please press new game to play again",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        disableAllButton()
     }
 
     fun buttonClick(view: View) {
         val btn = view as Button
-
         when (btn.id) {
             R.id.button1 -> {
-                if (player && !table[0]) {
+                if (player) {
+                    table[0] = 2
                     btn1.text = "x"
                     player = false
+                    if (checkHorizontal(1) || checkVertical(1) || checkCross(1)) {
+                        message()
+                    }
                 } else {
+                    table[0] = 1
                     btn1.text = "o"
                     player = true
+                    if (checkHorizontal(1) || checkVertical(1) || checkCross(1)) {
+                        message()
+                    }
                 }
-                table[0] = true
+                btn1.isClickable = false
             }
             R.id.button2 -> {
                 if (player) {
+                    table[1] = 2
                     btn2.text = "x"
                     player = false
+                    if (checkHorizontal(1) || checkVertical(2)) {
+                        message()
+                    }
                 } else {
+                    table[1] = 1
                     btn2.text = "o"
                     player = true
+                    if (checkHorizontal(1) || checkVertical(2)) {
+                        message()
+                    }
                 }
-                table[1] = true
+                btn2.isClickable = false
+
             }
             R.id.button3 -> {
                 if (player) {
+                    table[2] = 2
                     btn3.text = "x"
                     player = false
+                    if (checkHorizontal(1) || checkVertical(3) || checkCross(2)) {
+                        message()
+                    }
                 } else {
+                    table[2] = 1
                     btn3.text = "o"
                     player = true
+                    if (checkHorizontal(1) || checkVertical(3) || checkCross(2)) {
+                        message()
+                    }
                 }
-                table[2] = true
+                btn3.isClickable = false
             }
             R.id.button4 -> {
                 if (player) {
+                    table[3] = 2
                     btn4.text = "x"
                     player = false
+                    if (checkHorizontal(2) || checkVertical(1)) {
+                        message()
+                    }
                 } else {
+                    table[3] = 1
                     btn4.text = "o"
                     player = true
+                    if (checkHorizontal(2) || checkVertical(1)) {
+                        message()
+                    }
                 }
-                table[3] = true
+                btn4.isClickable = false
             }
             R.id.button5 -> {
                 if (player) {
+                    table[4] = 2
                     btn5.text = "x"
                     player = false
+                    if (checkHorizontal(2) || checkVertical(2) || checkCross(1) || checkCross(2)) {
+                        message()
+                    }
                 } else {
+                    table[4] = 1
                     btn5.text = "o"
                     player = true
+                    if (checkHorizontal(2) || checkVertical(2) || checkCross(1) || checkCross(2)) {
+                        message()
+                    }
                 }
-                table[4] = true
+                btn5.isClickable = false
             }
             R.id.button6 -> {
                 if (player) {
+                    table[5] = 2
                     btn6.text = "x"
                     player = false
+                    if (checkHorizontal(2) || checkVertical(3)) {
+                        message()
+                    }
                 } else {
+                    table[5] = 1
                     btn6.text = "o"
                     player = true
+                    if (checkHorizontal(2) || checkVertical(3) || checkCross(1)) {
+                        message()
+                    }
                 }
-                table[5] = true
+                btn6.isClickable = false
             }
             R.id.button7 -> {
                 if (player) {
+                    table[6] = 2
                     btn7.text = "x"
                     player = false
+                    if (checkHorizontal(3) || checkVertical(1) || checkCross(2)) {
+                        message()
+                    }
                 } else {
+                    table[6] = 1
                     btn7.text = "o"
                     player = true
+                    if (checkHorizontal(3) || checkVertical(1) || checkCross(2)) {
+                        message()
+                    }
                 }
-                table[6] = true
+                btn7.isClickable = false
             }
             R.id.button8 -> {
                 if (player) {
+                    table[7] = 2
                     btn8.text = "x"
                     player = false
+                    if (checkHorizontal(3) || checkVertical(2)) {
+                        message()
+                    }
                 } else {
+                    table[7] = 1
                     btn8.text = "o"
                     player = true
+                    if (checkHorizontal(3) || checkVertical(2)) {
+                        message()
+                    }
                 }
-                table[7] = true
+                btn8.isClickable = false
             }
             R.id.button9 -> {
                 if (player) {
+                    table[8] = 2
                     btn9.text = "x"
                     player = false
+                    if (checkHorizontal(3) || checkVertical(3) || checkCross(1)) {
+                        message()
+                    }
                 } else {
+                    table[8] = 1
                     btn9.text = "o"
                     player = true
+                    if (checkHorizontal(3) || checkVertical(3) || checkCross(1)) {
+                        message()
+                    }
                 }
-                table[8] = true
+                btn9.isClickable = false
             }
+        }
+        turn++
+        if (!won&&turn == 9) {
+            Toast.makeText(applicationContext, "It's a draw", Toast.LENGTH_SHORT).show()
         }
     }
 }
